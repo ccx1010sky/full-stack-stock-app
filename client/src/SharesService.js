2590f81a1aa0a7747b0f66125c90f110cb329589
ccx1010sky <ccx1010@gmail.com>
2022-02-06 15:47:18 +0000

    Delete TotalValueChartDate.js

--- client/src/SharesService.js
+++ client/src/SharesService.js
@@ -1,10 +1,19 @@
 const baseURL = 'http://localhost:5000/api/shares/'
 
+//Create,Read all,ReadById,UpdateById,DeleteById  functionality
+
+//get all shares
 export const getShares = () => {
     return fetch(baseURL)
         .then(res => res.json())
 }
 
+//get share by id
+export const getShare = (id) => {
+  return fetch(baseURL + id).then((res) => res.json());
+};
+
+//create new share
 export const postShare = (payload) => {
     return fetch(baseURL, {
         method: 'POST',
@@ -13,24 +22,19 @@ export const postShare = (payload) => {
     })
     .then(res => res.json())
 }
-
+//delete share by id
 export const deleteShare = (id) => {
     return fetch(baseURL + id, {
         method: 'DELETE'
     })
 }
 
-export const getShare = (id) => {
-  return fetch(baseURL + id)
-      .then(res => res.json())
-}
-
+//update share by id
 export const updateShare = (id, payload) => {
     return fetch(baseURL + id, {
         method: 'PUT',
         body: JSON.stringify(payload),
         headers: { 'Content-Type': 'application/json' }
     })
-    // .then(res => res.json())
 }
 
