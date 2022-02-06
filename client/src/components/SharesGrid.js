ac214209198692437f4467620670c49befaa178b
ccx1010sky <ccx1010@gmail.com>
2022-02-06 20:36:07 +0000

    delete TotalValueContainer.js

--- client/src/components/SharesGrid.js
+++ client/src/components/SharesGrid.js
@@ -1,23 +1,27 @@
 import React from "react";
 import ShareCard from "./ShareCard";
-import TotalValue from "./TotalValue";
 
+const SharesGrid = ({
+  removeShareFromDB,
+  shares,
+  updateShareNo,
+  getGraph,
+  chartType,
+}) => {
+  const sharesList = shares.map((share) => {
+    return (
+      <ShareCard
+        chartType={chartType}
+        share={share}
+        key={share._id}
+        getGraph={getGraph}
+        removeShareFromDB={removeShareFromDB}
+        updateShareNo={updateShareNo}
+      />
+    );
+  });
 
-const SharesGrid = ({removeShareFromDB, shares, updateShareNo, getGraph, chartType}) => {
-
-
-
-  const sharesList = shares.map(share => {
-  return <ShareCard chartType={chartType} share={share} key={share._id} getGraph={getGraph} removeShareFromDB={removeShareFromDB} updateShareNo={updateShareNo}/>
-})
-
-  return (
-    <div className="shares-grid">
-    {/* <TotalValue shares={shares}/> */}
-        {sharesList}
-    </div>
-  );
+  return <div className="shares-grid">{sharesList}</div>;
 };
 
-export default SharesGrid
-
+export default SharesGrid;
--- client/src/components/totalValueChart/TotalValueChart.js
+++ client/src/components/totalValueChart/TotalValueChart.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import TotalValueChartDesign from './TotalValueChartDesign';
 import './TotalValueChartDesign.css'
+
+
 const TotalValueChart = () => {
     const [totalShareData, setTotalShareData] = useState([])
     const [loading, setLoading] = useState(true)
