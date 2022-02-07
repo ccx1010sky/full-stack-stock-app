5a11ab600ad9c7c75b5429e29ba5eadca6f746c0
ccx1010sky <ccx1010@gmail.com>
2022-02-07 19:41:53 +0000

    format

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,29 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
   return (
     <>
-    
-    <ul className="navbar">
-      <div className="navbar-button">
-      <li>
-        <Link to="/"><button><i className="fas fa-home"></i> Home</button></Link>
-      </li>
-      </div>
-      <div className="navbar-button">
-      <li>
-        <Link to="/add"><button><i class="fas fa-plus-circle"></i> Add Shares</button></Link>
-      </li>
-      </div>
-      <div className="navbar-button">
-      <li>
-        <Link to="/calculator"><button><i class="fas fa-calculator"></i> Calculator</button></Link>
-      </li>
-      </div>
-    </ul>
+      <ul className="navbar">
+        <div className="navbar-button">
+          <li>
+            <Link to="/">
+              <button>
+                <i className="fas fa-home"></i> Home
+              </button>
+            </Link>
+          </li>
+        </div>
+        <div className="navbar-button">
+          <li>
+            <Link to="/add">
+              <button>
+                <i class="fas fa-plus-circle"></i> Add Shares
+              </button>
+            </Link>
+          </li>
+        </div>
+        <div className="navbar-button">
+          <li>
+            <Link to="/calculator">
+              <button>
+                <i class="fas fa-calculator"></i> Calculator
+              </button>
+            </Link>
+          </li>
+        </div>
+      </ul>
     </>
   );
-}
+};
 
 export default NavBar;
